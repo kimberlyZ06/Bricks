b892992ece09dde747bef93567150d417e3a101a
U1_Projecyt <kimberlyz26@nycstudents.net>
2026-04-12 20:25:17 -0400

    :(

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         System.out.println(partOne(bricks));
         System.out.println(partTwo(bricks));
-//        System.out.println(part2dArray(bricks));
+        System.out.println(part2dArray(bricks));
     }
 
     private static int partOne(ArrayList<Brick> bricks) {
@@ -36,16 +36,16 @@ public class Main {
         checked.add(bricks.get(0));
         for (int i = 1; i < bricks.size(); i++) {
             Brick current = bricks.get(i);
-            System.out.println("Current brick: " + current);
+//            System.out.println("Current brick: " + current);
             for (int j = 0; j < checked.size(); j++) {
-                System.out.println("Current Check: " + checked.get(j));
+//                System.out.println("Current Check: " + checked.get(j));
                 if(current.overlap(current,checked.get(j))){
                     bricks.get(i).setLayer(checked.get(j).getLayer() + 1);
-                    System.out.println("Current brick layer: " + bricks.get(i).getLayer());
+//                    System.out.println("Current brick layer: " + bricks.get(i).getLayer());
                 }
             }
             checked.add(bricks.get(i));
-            System.out.println(checked);
+//            System.out.println(checked);
         }
 
         int height = 0;
@@ -58,42 +58,50 @@ public class Main {
         return height;
     }
 
-//    private static int part2dArray(ArrayList<Brick> bricks) {
-//        int maxX = 0;
-//        for (int i = 0; i < bricks.size(); i++) {
-//            int currentX = bricks.get(i).getEnd();
-//            if (currentX > maxX){
-//                maxX = currentX;
-//            }
-//        }
-//        int maxY = bricks.size();
-//        int[][] array = new int[maxY][maxX];
-//        for (int i = 0; i < bricks.size(); i++) {
-//            bricks.get(i).setLayer(0);
-//        }
-//        for (int i = 0; i < bricks.size(); i++) {
-//            for (int j = bricks.get(i).getStart(); i <= bricks.get(i).getEnd(); i++) {
-//                array[bricks.get(i).getLayer()][j] = 1;
-//            }
-//        }
-//
-//        int height = array.length;
-//        System.out.println(height);
-//        for (int i = array.length - 1; i >= 0 ; i--) {
-//            boolean empty = true;
-//            if (array[i].toString().contains("1")){
-//                empty = false;
-//            }
-//            System.out.println(empty);
-//
-//            if (empty){
-//                height--;
-//            }
-//        }
-//
-//
-//        return height;
-//    }
+    private static int part2dArray(ArrayList<Brick> bricks) {
+        int maxX = 0;
+        for (int i = 0; i < bricks.size(); i++) {
+            int currentX = bricks.get(i).getEnd();
+            if (currentX > maxX){
+                maxX = currentX;
+            }
+        }
+        int maxY = bricks.size();
+        int[][] array = new int[maxY][maxX];
+
+        int layer = 0;
+        for (int i = 0; i < bricks.size(); i++) {
+            bricks.get(i).setLayer(layer);
+            for (int j = bricks.get(i).getStart(); j <= bricks.get(i).getEnd(); j++) {
+                array[bricks.get(i).getLayer()][j - 1] = 1;
+            }
+            Brick current = bricks.get(i);
+            if(i != 0) {
+                System.out.println("Current brick: " + current);
+                System.out.println("Previous brick: " + bricks.get(i - 1));
+            }
+            if(i > 0) {
+                if (current.overlap(current, bricks.get(i - 1))) {
+                    System.out.println(current + " overlaps w/ " + bricks.get(i-1));
+                    layer++;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(array));
+
+        int height = array.length;
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            boolean empty = true;
+            if (array[i].toString().contains("1")){
+                empty = false;
+            }
+
+            if (empty){
+                height--;
+            }
+        }
+        return height;
+    }
 
     public static ArrayList<String> getFileData(String fileName) {
         File f = new File(fileName);
